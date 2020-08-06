silvasean/iree

Refactoring VM register allocation to support more types.

iree/compiler/Dialect/VM/Analysis/RegisterAllocation.h
@@ -18,57 +18,150 @@
 #include "iree/compiler/Dialect/VM/Analysis/ValueLiveness.h"
 #include "iree/compiler/Dialect/VM/IR/VMOps.h"
 #include "llvm/ADT/DenseMap.h"
+#include "llvm/ADT/DenseMapInfo.h"
 #include "mlir/IR/Operation.h"
 
 namespace mlir {
 namespace iree_compiler {
 
+// Represents a register value at a particular usage.
+//
 // The VM contains multiple register banks:
-// - 128 32-bit integer registers
-//   - may be aliased as 32 128-bit registers
-// - 64 ref_ptr registers
+// - 32-bit integer registers
+//   - may be aliased as 64/128/etc-bit registers
+// - ref_ptr registers
 //
-// Registers are represented in bytecode as an 8-bit integer with the high bit
-// indicating whether it is from the integer (0b0) or ref_ptr bank (0b1).
+// Registers are represented in bytecode as an N-bit integer with the high bit
+// indicating whether it is from the general (0b0) or ref_ptr bank (0b1).
 //
-// ref_ptr register bytes also include a bit denoting whether the register
+// ref_ptr register ordinals also include a bit denoting whether the register
 // reference has move semantics. When set the VM can assume that the value is
 // no longer used in the calling code and that ownership can be transferred to
 // the receiving op/call. This allows reference count increment elision, though
 // the VM is free to ignore this if it so chooses.
+class Register {
+ public:
+  static constexpr int kIntRegisterCount = 0x7FFF;
+  static constexpr int kRefRegisterCount = 0x3FFF;
+  static constexpr uint16_t kRefTypeBit = 0x8000;
+  static constexpr uint16_t kRefMoveBit = 0x4000;
+
+  // Returns a ref-type register with an optional move bit set.
+  static Register getRef(Type type, int ordinal, bool isMove = false) {
+    return {/*isRef=*/true, isMove, /*byteWidth=*/0, ordinal};
+  }
+
+  // Returns a value-type register with a bit-width derived from |type|.
+  static Register getValue(Type type, int ordinal) {
+    assert(type.isSignlessIntOrIndexOrFloat());
+    assert(ordinal < kIntRegisterCount);
+    // DO NOT SUBMIT byteWidth assert that ordinal is clear and meets
+    // alignment
+    size_t byteWidth = 4;
+    return {/*isRef=*/false, /*isMove=*/false, byteWidth, ordinal};
+  }
+
+  // Returns a register with the same type as |other| but with the new
+  // |ordinal|.
+  static Register getWithSameType(Register other, int ordinal) {
+    return {other.isRef(), /*isMove=*/false, other.byteWidth(), ordinal};
+  }
+
+  static Register getEmptyKey() { return Register(); }
+  static Register getTombstoneKey() {
+    auto reg = Register();
+    reg.null_ = 0;
+    reg.tombstone_ = 1;
+    return reg;
+  }
+
+  Register()
+      : null_(1),
+        tombstone_(0),
+        isRef_(0),
+        isMove_(0),
+        byteWidth_(0),
+        reserved_(0),
+        ordinal_(0) {}
+
+  // Returns the register without any usage-specific bits set (such as move).
+  Register asBaseRegister() const {
+    return {isRef(), false, byteWidth(), ordinal()};
+  }
+
+  constexpr bool isRef() const { return isRef_; }
+  constexpr bool isMove() const { return isMove_; }
+  void setMove(bool isMove) { isMove_ = isMove ? 1 : 0; }
+
+  constexpr bool isValue() const { return !isRef_; }
+  constexpr size_t byteWidth() const { return byteWidth_; }
+
+  // 0-N ordinal within the register bank.
+  constexpr uint16_t ordinal() const { return ordinal_; }
+
+  // Encodes the register into a uint16_t value used by the runtime VM.
+  uint16_t encode() const {
+    assert(!null_ && !tombstone_ && "cannot encode a sentinel register");
+    if (isRef()) {
+      return kRefTypeBit | (isMove() ? kRefMoveBit : 0) | ordinal();
+    } else {
+      return ordinal();
+    }
+  }
+
+  std::string toString() const {
+    if (null_ || tombstone_) {
+      return "<invalid>";
+    } else if (isRef()) {
+      std::string result = isMove() ? "R" : "r";
+      return result + std::to_string(ordinal());
+    } else {
+      return std::string("i") + std::to_string(ordinal());
+    }
+  }
+
+  unsigned getHashValue() const { return hashValue_; }
+
+  // Compares two registers excluding the move bit.
+  bool operator==(const Register &other) const {
+    if (null_ != other.null_ || tombstone_ != other.tombstone_) {
+      return false;
+    }
+    if (isRef() != other.isRef() || ordinal() != other.ordinal()) {
+      return false;
+    } else if (isRef()) {
+      return true;
+    } else {
+      return byteWidth() == other.byteWidth();
+    }
+  }
+  bool operator!=(const Register &other) const { return !(*this == other); }
+
+ private:
+  Register(bool isRef, bool isMove, size_t byteWidth, int ordinal)
+      : null_(0),
+        tombstone_(0),
+        isRef_(isRef),
+        isMove_(isMove),
+        byteWidth_(byteWidth),
+        ordinal_(ordinal) {}
+
+  union {
+    struct {
+      uint16_t null_ : 1;  // 1 if the register is indicating an empty value
+      uint16_t tombstone_ : 1;  // 1 if a DenseMap tombstone value
+      uint16_t isRef_ : 1;
+      uint16_t isMove_ : 1;
+      uint16_t byteWidth_ : 8;
+      uint16_t reserved_ : 4;
+      uint16_t ordinal_ : 16;
+    };
+    uint32_t hashValue_;
+  };
+};
 
 constexpr int kIntRegisterCount = 0x7FFF;
 constexpr int kRefRegisterCount = 0x3FFF;
-constexpr uint16_t kRefRegisterTypeBit = 0x8000;
-constexpr uint16_t kRefRegisterMoveBit = 0x4000;
-
-// Returns true if |reg| is a register in the ref_ptr bank.
-constexpr bool isRefRegister(uint16_t reg) {
-  return (reg & kRefRegisterTypeBit) == kRefRegisterTypeBit;
-}
-
-// Returns true if the ref_ptr |reg| denotes a move operation.
-constexpr bool isRefMove(uint16_t reg) {
-  return (reg & kRefRegisterMoveBit) == kRefRegisterMoveBit;
-}
-
-// Returns the register 0-based ordinal within its set.
-constexpr uint16_t getRegisterOrdinal(uint16_t reg) {
-  return isRefRegister(reg)
-             ? (reg & ~(kRefRegisterTypeBit | kRefRegisterMoveBit))
-             : reg;
-}
-
-// Returns the register ID without the move bit.
-constexpr uint16_t getBaseRegister(uint16_t reg) {
-  return isRefRegister(reg) ? (reg & ~kRefRegisterMoveBit) : reg;
-}
-
-// Compares whether two register bytes are equal to each other, ignoring any
-// move semantics on ref_ptr registers.
-constexpr bool compareRegistersEqual(uint16_t a, uint16_t b) {
-  return getBaseRegister(a) == getBaseRegister(b);
-}
 
 // Analysis that performs VM register allocation on the given function op and
 // its children. Once calculated value usages can be mapped to VM register
@@ -94,25 +187,25 @@ class RegisterAllocation {
 
   // Maximum allocated register ordinals.
   // May be -1 if no registers of the specific type were allocated.
-  uint16_t getMaxI32RegisterOrdinal() {
+  int getMaxI32RegisterOrdinal() {
     return maxI32RegisterOrdinal_ + scratchI32RegisterCount_;
   }
-  uint16_t getMaxRefRegisterOrdinal() {
+  int getMaxRefRegisterOrdinal() {
     return maxRefRegisterOrdinal_ + scratchRefRegisterCount_;
   }
 
   // Maps a |value| to a register with no move bit set.
   // Prefer mapUseToRegister when a move is desired.
-  uint16_t mapToRegister(Value value);
+  Register mapToRegister(Value value);
 
   // Maps a |value| to a register as calculated during allocation. The returned
   // register will have the proper type and move bits set.
-  uint16_t mapUseToRegister(Value value, Operation *useOp, int operandIndex);
+  Register mapUseToRegister(Value value, Operation *useOp, int operandIndex);
 
   // Remaps branch successor operands to the target block argument registers.
   // Returns a list of source to target register mappings. Source ref registers
   // may have their move bit set.
-  SmallVector<std::pair<uint16_t, uint16_t>, 8> remapSuccessorRegisters(
+  SmallVector<std::pair<Register, Register>, 8> remapSuccessorRegisters(
       Operation *op, int successorIndex);
 
  private:
@@ -125,10 +218,31 @@ class RegisterAllocation {
   ValueLiveness liveness_;
 
   // Mapping from all values within the operation to registers.
-  llvm::DenseMap<Value, uint16_t> map_;
+  llvm::DenseMap<Value, Register> map_;
 };
 
 }  // namespace iree_compiler
 }  // namespace mlir
+
+namespace llvm {
+template <>
+struct DenseMapInfo<mlir::iree_compiler::Register> {
+  using Register = mlir::iree_compiler::Register;
+
+  static inline Register getEmptyKey() { return Register::getEmptyKey(); }
+
+  static inline Register getTombstoneKey() {
+    return Register::getTombstoneKey();
+  }
+
+  static unsigned getHashValue(const Register &val) {
+    return val.getHashValue();
+  }
+
+  static bool isEqual(const Register &lhs, const Register &rhs) {
+    return lhs == rhs;
+  }
+};
+}  // namespace llvm
 
 #endif  // IREE_COMPILER_DIALECT_VM_ANALYSIS_REGISTERALLOCATION_H_

GMNGeoffrey/iree-clean

iree/compiler/Dialect/VM/Analysis/RegisterAllocation.h
@@ -18,57 +18,152 @@
 #include "iree/compiler/Dialect/VM/Analysis/ValueLiveness.h"
 #include "iree/compiler/Dialect/VM/IR/VMOps.h"
 #include "llvm/ADT/DenseMap.h"
+#include "llvm/ADT/DenseMapInfo.h"
 #include "mlir/IR/Operation.h"
 
 namespace mlir {
 namespace iree_compiler {
 
+// Represents a register value at a particular usage.
+//
 // The VM contains multiple register banks:
-// - 128 32-bit integer registers
-//   - may be aliased as 32 128-bit registers
-// - 64 ref_ptr registers
+// - 32-bit integer registers
+//   - may be aliased as 64/128/etc-bit registers
+// - ref_ptr registers
 //
-// Registers are represented in bytecode as an 8-bit integer with the high bit
-// indicating whether it is from the integer (0b0) or ref_ptr bank (0b1).
+// Registers are represented in bytecode as an N-bit integer with the high bit
+// indicating whether it is from the general (0b0) or ref_ptr bank (0b1).
 //
-// ref_ptr register bytes also include a bit denoting whether the register
+// ref_ptr register ordinals also include a bit denoting whether the register
 // reference has move semantics. When set the VM can assume that the value is
 // no longer used in the calling code and that ownership can be transferred to
 // the receiving op/call. This allows reference count increment elision, though
 // the VM is free to ignore this if it so chooses.
+class Register {
+ public:
+  static constexpr int kInt32RegisterCount = 0x7FFF;
+  static constexpr int kRefRegisterCount = 0x3FFF;
+  static constexpr uint16_t kRefTypeBit = 0x8000;
+  static constexpr uint16_t kRefMoveBit = 0x4000;
 
-constexpr int kIntRegisterCount = 0x7FFF;
-constexpr int kRefRegisterCount = 0x3FFF;
-constexpr uint16_t kRefRegisterTypeBit = 0x8000;
-constexpr uint16_t kRefRegisterMoveBit = 0x4000;
+  // Returns a ref-type register with an optional move bit set.
+  static Register getRef(Type type, int ordinal, bool isMove = false) {
+    return {/*isRef=*/true, isMove, /*byteWidth=*/0, ordinal};
+  }
 
-// Returns true if |reg| is a register in the ref_ptr bank.
-constexpr bool isRefRegister(uint16_t reg) {
-  return (reg & kRefRegisterTypeBit) == kRefRegisterTypeBit;
-}
+  // Returns a value-type register with a bit-width derived from |type|.
+  static Register getValue(Type type, int ordinal) {
+    assert(type.isIntOrFloat() && "require int/float (no index)");
+    assert(type.getIntOrFloatBitWidth() % 8 == 0 &&
+           "require 8-bit aligned value types");
+    assert(ordinal < kInt32RegisterCount);
+    size_t byteWidth = type.getIntOrFloatBitWidth() / 8;
+    return {/*isRef=*/false, /*isMove=*/false, byteWidth, ordinal};
+  }
 
-// Returns true if the ref_ptr |reg| denotes a move operation.
-constexpr bool isRefMove(uint16_t reg) {
-  return (reg & kRefRegisterMoveBit) == kRefRegisterMoveBit;
-}
+  // Returns a register with the same type as |other| but with the new
+  // |ordinal|.
+  static Register getWithSameType(Register other, int ordinal) {
+    return {other.isRef(), /*isMove=*/false, other.byteWidth(), ordinal};
+  }
 
-// Returns the register 0-based ordinal within its set.
-constexpr uint16_t getRegisterOrdinal(uint16_t reg) {
-  return isRefRegister(reg)
-             ? (reg & ~(kRefRegisterTypeBit | kRefRegisterMoveBit))
-             : reg;
-}
+  static Register getEmptyKey() { return Register(); }
+  static Register getTombstoneKey() {
+    auto reg = Register();
+    reg.null_ = 0;
+    reg.tombstone_ = 1;
+    return reg;
+  }
 
-// Returns the register ID without the move bit.
-constexpr uint16_t getBaseRegister(uint16_t reg) {
-  return isRefRegister(reg) ? (reg & ~kRefRegisterMoveBit) : reg;
-}
+  Register()
+      : null_(1),
+        tombstone_(0),
+        isRef_(0),
+        isMove_(0),
+        byteWidth_(0),
+        reserved_(0),
+        ordinal_(0) {}
 
-// Compares whether two register bytes are equal to each other, ignoring any
-// move semantics on ref_ptr registers.
-constexpr bool compareRegistersEqual(uint16_t a, uint16_t b) {
-  return getBaseRegister(a) == getBaseRegister(b);
-}
+  // Returns the register without any usage-specific bits set (such as move).
+  Register asBaseRegister() const {
+    return {isRef(), false, byteWidth(), ordinal()};
+  }
+
+  constexpr bool isRef() const { return isRef_; }
+  constexpr bool isMove() const { return isMove_; }
+  void setMove(bool isMove) { isMove_ = isMove ? 1 : 0; }
+
+  constexpr bool isValue() const { return !isRef_; }
+  constexpr size_t byteWidth() const { return byteWidth_; }
+
+  // 0-N ordinal within the register bank.
+  constexpr uint16_t ordinal() const { return ordinal_; }
+
+  // Encodes the register into a uint16_t value used by the runtime VM.
+  uint16_t encode() const {
+    assert(!null_ && !tombstone_ && "cannot encode a sentinel register");
+    if (isRef()) {
+      return kRefTypeBit | (isMove() ? kRefMoveBit : 0) | ordinal();
+    } else {
+      return ordinal();
+    }
+  }
+
+  std::string toString() const {
+    if (null_ || tombstone_) {
+      return "<invalid>";
+    } else if (isRef()) {
+      std::string result = isMove() ? "R" : "r";
+      return result + std::to_string(ordinal());
+    } else if (byteWidth() == 8) {
+      return std::string("i") + std::to_string(ordinal()) + "+" +
+             std::to_string(ordinal() + 1);
+    } else if (byteWidth() == 4) {
+      return std::string("i") + std::to_string(ordinal());
+    } else {
+      return "<unknown>";
+    }
+  }
+
+  unsigned getHashValue() const { return hashValue_; }
+
+  // Compares two registers excluding the move bit.
+  bool operator==(const Register &other) const {
+    if (null_ != other.null_ || tombstone_ != other.tombstone_) {
+      return false;
+    }
+    if (isRef() != other.isRef() || ordinal() != other.ordinal()) {
+      return false;
+    } else if (isRef()) {
+      return true;
+    } else {
+      return byteWidth() == other.byteWidth();
+    }
+  }
+  bool operator!=(const Register &other) const { return !(*this == other); }
+
+ private:
+  Register(bool isRef, bool isMove, size_t byteWidth, int ordinal)
+      : null_(0),
+        tombstone_(0),
+        isRef_(isRef),
+        isMove_(isMove),
+        byteWidth_(byteWidth),
+        ordinal_(ordinal) {}
+
+  union {
+    struct {
+      uint16_t null_ : 1;  // 1 if the register is indicating an empty value
+      uint16_t tombstone_ : 1;  // 1 if a DenseMap tombstone value
+      uint16_t isRef_ : 1;
+      uint16_t isMove_ : 1;
+      uint16_t byteWidth_ : 8;
+      uint16_t reserved_ : 4;
+      uint16_t ordinal_ : 16;
+    };
+    uint32_t hashValue_;
+  };
+};
 
 // Analysis that performs VM register allocation on the given function op and
 // its children. Once calculated value usages can be mapped to VM register
@@ -94,25 +189,25 @@ class RegisterAllocation {
 
   // Maximum allocated register ordinals.
   // May be -1 if no registers of the specific type were allocated.
-  uint16_t getMaxI32RegisterOrdinal() {
+  int getMaxI32RegisterOrdinal() {
     return maxI32RegisterOrdinal_ + scratchI32RegisterCount_;
   }
-  uint16_t getMaxRefRegisterOrdinal() {
+  int getMaxRefRegisterOrdinal() {
     return maxRefRegisterOrdinal_ + scratchRefRegisterCount_;
   }
 
   // Maps a |value| to a register with no move bit set.
   // Prefer mapUseToRegister when a move is desired.
-  uint16_t mapToRegister(Value value);
+  Register mapToRegister(Value value);
 
   // Maps a |value| to a register as calculated during allocation. The returned
   // register will have the proper type and move bits set.
-  uint16_t mapUseToRegister(Value value, Operation *useOp, int operandIndex);
+  Register mapUseToRegister(Value value, Operation *useOp, int operandIndex);
 
   // Remaps branch successor operands to the target block argument registers.
   // Returns a list of source to target register mappings. Source ref registers
   // may have their move bit set.
-  SmallVector<std::pair<uint16_t, uint16_t>, 8> remapSuccessorRegisters(
+  SmallVector<std::pair<Register, Register>, 8> remapSuccessorRegisters(
       Operation *op, int successorIndex);
 
  private:
@@ -125,10 +220,31 @@ class RegisterAllocation {
   ValueLiveness liveness_;
 
   // Mapping from all values within the operation to registers.
-  llvm::DenseMap<Value, uint16_t> map_;
+  llvm::DenseMap<Value, Register> map_;
 };
 
 }  // namespace iree_compiler
 }  // namespace mlir
+
+namespace llvm {
+template <>
+struct DenseMapInfo<mlir::iree_compiler::Register> {
+  using Register = mlir::iree_compiler::Register;
+
+  static inline Register getEmptyKey() { return Register::getEmptyKey(); }
+
+  static inline Register getTombstoneKey() {
+    return Register::getTombstoneKey();
+  }
+
+  static unsigned getHashValue(const Register &val) {
+    return val.getHashValue();
+  }
+
+  static bool isEqual(const Register &lhs, const Register &rhs) {
+    return lhs == rhs;
+  }
+};
+}  // namespace llvm
 
 #endif  // IREE_COMPILER_DIALECT_VM_ANALYSIS_REGISTERALLOCATION_H_