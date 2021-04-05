iree-org/iree

commit ce0285fe5996cf679bb667f5320869e55ddf1866

Continue pruning abseil usage: switch from absl::InlinedVector to std::vector. (#5302)
Also switched to using `IREE_ATTRIBUTE_NOINLINE` from `ABSL_ATTRIBUTE_NOINLINE`.

Generated via a pile of regex (code, includes, build deps) and manual cleanup until builds passed again.

* native modules needed some extra changes to work around calling `iree_hal_buffer_view_shape` with rank 0

iree/modules/vmla/op_kernels_generic.h
@@ -20,9 +20,9 @@
 #include <iostream>
 #include <iterator>
 #include <numeric>
+#include <vector>
 
 #include "absl/container/flat_hash_set.h"
-#include "absl/container/inlined_vector.h"
 #include "absl/types/span.h"
 #include "iree/base/status.h"
 
@@ -92,9 +92,9 @@ Status CompareGE::Execute(absl::Span<const T> lhs_buffer,
 }
 
 namespace impl {
-inline absl::InlinedVector<size_t, 6> ComputeCopyStrides(ShapeSpan shape,
-                                                         size_t element_size) {
-  absl::InlinedVector<size_t, 6> strides(shape.size());
+inline std::vector<size_t> ComputeCopyStrides(ShapeSpan shape,
+                                              size_t element_size) {
+  std::vector<size_t> strides(shape.size());
   strides.back() = element_size;
   for (int i = static_cast<int>(shape.size()) - 2; i >= 0; --i) {
     strides[i] = strides[i + 1] * shape[i + 1];
@@ -276,9 +276,9 @@ Status Transpose::Execute(absl::Span<const T> src_buffer,
                           absl::Span<const int32_t> perm) {
   int rank = src_shape.size();
 
-  absl::InlinedVector<int, 8> src_strides(rank);
-  absl::InlinedVector<int, 8> dst_strides(rank);
-  absl::InlinedVector<int32_t, 8> dst_shape(rank);
+  std::vector<int> src_strides(rank);
+  std::vector<int> dst_strides(rank);
+  std::vector<int32_t> dst_shape(rank);
   size_t src_stride = 1;
   size_t dst_stride = 1;
   for (int dim_i = rank - 1; dim_i >= 0; --dim_i) {
@@ -348,7 +348,7 @@ Status Pad::Execute(absl::Span<const T> src_buffer,
   }
   auto padding_value = padding_value_buffer.front();
 
-  absl::InlinedVector<int, 8> dst_indices(src_shape.size(), 0);
+  std::vector<int> dst_indices(src_shape.size(), 0);
 
   const T* src_ptr = src_buffer.begin();
   T* dst_ptr = dst_buffer.begin();
@@ -536,7 +536,7 @@ Status Reverse::Execute(absl::Span<const T> src_buffer,
                         absl::Span<const int32_t> dimensions) {
   // This implementation is not fast either.
   int rank = src_shape.size();
-  absl::InlinedVector<int, 8> strides(rank);
+  std::vector<int> strides(rank);
   size_t stride = 1;
   for (int dim_i = rank - 1; dim_i >= 0; --dim_i) {
     strides[dim_i] = stride;
@@ -601,8 +601,8 @@ Status Tile::Execute(absl::Span<const T> src_buffer, absl::Span<T> dst_buffer,
                      ShapeSpan src_shape, ShapeSpan dst_shape) {
   // This implementation is .... not fast.
   int rank = dst_shape.size();
-  absl::InlinedVector<int, 8> src_strides(rank);
-  absl::InlinedVector<int, 8> dst_strides(rank);
+  std::vector<int> src_strides(rank);
+  std::vector<int> dst_strides(rank);
   size_t src_stride = 1;
   size_t dst_stride = 1;
   for (int dim_i = rank - 1; dim_i >= 0; --dim_i) {
@@ -960,7 +960,7 @@ inline void ReduceDimension(absl::Span<const T> src_buffer,
     //
     // TODO(scotttodd): Clean this up somehow, share across recursion levels?
     size_t dst_size = src_shape.size() - reduce_dims.size();
-    absl::InlinedVector<int, 8> dst_indices;
+    std::vector<int> dst_indices;
     for (size_t i = 0; i < src_indices.size(); ++i) {
       if (std::find(std::begin(reduce_dims), std::end(reduce_dims), i) ==
           std::end(reduce_dims)) {
@@ -1012,7 +1012,7 @@ Status GenericReduce(absl::Span<const T> src_buffer,
 
   // Precompute destination strides.
   int dst_rank = dst_shape.size();
-  absl::InlinedVector<int, 8> dst_strides;
+  std::vector<int> dst_strides;
   size_t dst_stride = 1;
   for (int dim_i = dst_rank - 1; dim_i >= 0; --dim_i) {
     dst_strides.push_back(dst_stride);
@@ -1024,7 +1024,7 @@ Status GenericReduce(absl::Span<const T> src_buffer,
   //   * the innermost dimension (last in the shape)
   //   * flat_src_i of 0 (corresponds to [0, 0, ..., 0] above)
   //   * source stride 1
-  absl::InlinedVector<int, 8> src_indices(src_shape.size(), 0);
+  std::vector<int> src_indices(src_shape.size(), 0);
   ReduceDimension<T, KernelImpl>(src_buffer, dst_buffer, src_shape, {dimension},
                                  absl::MakeSpan(dst_strides),
                                  src_shape.size() - 1,
@@ -1088,7 +1088,7 @@ void ComputePoolingWindow(absl::Span<const T> src_buffer,
                           ShapeSpan src_shape, T init_value,
                           ShapeSpan window_dimensions, T* dst_value) {
   size_t rank = src_shape.size();
-  absl::InlinedVector<int, 8> window_indices(rank, 0);
+  std::vector<int> window_indices(rank, 0);
   auto getSrcValue = [&]() -> T {
     size_t flat_idx = 0;
     for (size_t i = 0; i < rank; ++i) {
@@ -1113,8 +1113,8 @@ Status GenericPooling(absl::Span<const T> src_buffer,
                       ShapeSpan window_dimensions, ShapeSpan strides,
                       ShapeSpan pad_low) {
   size_t rank = src_shape.size();
-  absl::InlinedVector<int, 8> src_indices(rank, 0);
-  absl::InlinedVector<int, 8> dst_indices(rank, 0);
+  std::vector<int> src_indices(rank, 0);
+  std::vector<int> dst_indices(rank, 0);
   for (size_t i = 0, e = GetElementCount(dst_shape); i < e; ++i) {
     for (size_t j = 0; j < rank; ++j) {
       src_indices[j] = dst_indices[j] * strides[j] - pad_low[j];

iree/base/BUILD
@@ -97,7 +97,6 @@ cc_library(
     srcs = ["signature_parser.cc"],
     hdrs = ["signature_parser.h"],
     deps = [
-        "@com_google_absl//absl/container:inlined_vector",
         "@com_google_absl//absl/strings",
         "@com_google_absl//absl/types:optional",
         "@com_google_absl//absl/types:span",

iree/base/CMakeLists.txt
@@ -93,7 +93,6 @@ iree_cc_library(
   SRCS
     "signature_parser.cc"
   DEPS
-    absl::inlined_vector
     absl::optional
     absl::span
     absl::strings

iree/base/signature_parser.h
@@ -19,8 +19,8 @@
 #include <cassert>
 #include <map>
 #include <string>
+#include <vector>
 
-#include "absl/container/inlined_vector.h"
 #include "absl/strings/string_view.h"
 #include "absl/types/optional.h"
 #include "absl/types/span.h"
@@ -112,7 +112,7 @@ class SignatureParser {
 // Parses function signatures generated by RawSignatureMangler.
 class RawSignatureParser {
  public:
-  using DimVector = absl::InlinedVector<int, 4>;
+  using DimVector = std::vector<int>;
 
   enum class Type {
     kBuffer = 0,

iree/hal/BUILD
@@ -78,7 +78,6 @@ cc_library(
         "//iree/base:threading",
         "//iree/base:tracing",
         "//iree/base/internal",
-        "@com_google_absl//absl/container:inlined_vector",
         "@com_google_absl//absl/strings",
         "@com_google_absl//absl/types:span",
         "@half//:includes",
@@ -94,7 +93,7 @@ cc_test(
         "//iree/base:status",
         "//iree/testing:gtest",
         "//iree/testing:gtest_main",
-        "@com_google_absl//absl/container:inlined_vector",
         "@com_google_absl//absl/strings",
+        "@com_google_absl//absl/types:span",
     ],
 )

iree/hal/CMakeLists.txt
@@ -53,7 +53,6 @@ iree_cc_library(
     "string_util.cc"
     "string_util.h"
   DEPS
-    absl::inlined_vector
     absl::span
     absl::strings
     half::includes
@@ -73,7 +72,7 @@ iree_cc_test(
     "string_util_test.cc"
   DEPS
     ::api
-    absl::inlined_vector
+    absl::span
     absl::strings
     iree::base::core_headers
     iree::base::status

iree/hal/buffer_view.cc
@@ -17,8 +17,8 @@
 #include <cctype>
 #include <cinttypes>
 #include <cstdio>
+#include <vector>
 
-#include "absl/container/inlined_vector.h"
 #include "absl/strings/ascii.h"
 #include "absl/strings/match.h"
 #include "absl/strings/numbers.h"
@@ -75,7 +75,7 @@ IREE_API_EXPORT iree_status_t IREE_API_CALL iree_hal_buffer_view_parse(
   }
 
   // AxBxC...
-  absl::InlinedVector<iree_hal_dim_t, 6> shape(6);
+  std::vector<iree_hal_dim_t> shape(6);
   iree_host_size_t shape_rank = 0;
   iree_status_t shape_result =
       iree_hal_parse_shape({shape_str.data(), shape_str.length()}, shape.size(),

iree/hal/string_util.cc
@@ -17,8 +17,8 @@
 #include <cctype>
 #include <cinttypes>
 #include <cstdio>
+#include <vector>
 
-#include "absl/container/inlined_vector.h"
 #include "absl/strings/ascii.h"
 #include "absl/strings/match.h"
 #include "absl/strings/numbers.h"
@@ -44,7 +44,7 @@ IREE_API_EXPORT iree_status_t IREE_API_CALL iree_hal_parse_shape(
     return iree_ok_status();  // empty shape
   }
 
-  absl::InlinedVector<iree_hal_dim_t, 6> dims;
+  std::vector<iree_hal_dim_t> dims;
   for (auto dim_str : absl::StrSplit(str_value, 'x')) {
     int dim_value = 0;
     if (!absl::SimpleAtoi(dim_str, &dim_value)) {

GMNGeoffrey/iree-clean

commit 66e872741cd1d790b555ff6192721de7e67c3579

Patch VMLA performance by reserving vector size before pushing to it. (#5316)
Addressing a performance regression introduced by https://github.com/google/iree/pull/5302

![image](https://user-images.githubusercontent.com/4010439/113621630-b5487580-9610-11eb-930b-961ada1755b8.png)

iree/modules/vmla/op_kernels_generic.h
@@ -961,6 +961,7 @@ inline void ReduceDimension(absl::Span<const T> src_buffer,
     // TODO(scotttodd): Clean this up somehow, share across recursion levels?
     size_t dst_size = src_shape.size() - reduce_dims.size();
     std::vector<int> dst_indices;
+    dst_indices.reserve(src_indices.size());
     for (size_t i = 0; i < src_indices.size(); ++i) {
       if (std::find(std::begin(reduce_dims), std::end(reduce_dims), i) ==
           std::end(reduce_dims)) {