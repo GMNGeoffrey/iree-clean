pashu123/iree

Adding iree_hal_buffer_view_generate_buffer. This allows for us to have one code path for UMA and discrete buffer view initialization without the need to allocate or copy. Real applications may want to do this in specific cases but it'll mostly be useful for covering parsing/generation of data in tests and tools.

iree/tools/utils/trace_replay.c
@@ -468,7 +468,7 @@ static iree_status_t iree_trace_replay_parse_hal_encoding_type(
 static iree_status_t iree_trace_replay_parse_hal_buffer(
     iree_trace_replay_t* replay, yaml_document_t* document,
     yaml_node_t* contents_node, iree_hal_element_type_t element_type,
-    iree_hal_buffer_t* buffer) {
+    iree_hal_buffer_mapping_t* mapping) {
   if (!contents_node) {
     // Empty contents = zero fill.
     return iree_ok_status();
@@ -480,23 +480,17 @@ static iree_status_t iree_trace_replay_parse_hal_buffer(
   iree_string_view_t value =
       iree_string_view_trim(iree_yaml_node_as_string(contents_node));
 
-  iree_hal_buffer_mapping_t mapping;
-  IREE_RETURN_IF_ERROR(
-      iree_hal_buffer_map_range(buffer, IREE_HAL_MEMORY_ACCESS_DISCARD_WRITE, 0,
-                                IREE_WHOLE_BUFFER, &mapping));
   iree_status_t status = iree_ok_status();
   if (strcmp(contents_node->tag, "tag:yaml.org,2002:binary") == 0) {
-    status = iree_yaml_base64_decode(value, mapping.contents);
+    return iree_yaml_base64_decode(value, mapping->contents);
   } else if (strcmp(contents_node->tag, "tag:yaml.org,2002:str") == 0) {
-    status =
-        iree_hal_parse_buffer_elements(value, element_type, mapping.contents);
+    return iree_hal_parse_buffer_elements(value, element_type,
+                                          mapping->contents);
   } else {
-    status = iree_make_status(
-        IREE_STATUS_UNIMPLEMENTED, "(%zu): unimplemented buffer encoding '%s'",
-        contents_node->start_mark.line, contents_node->tag);
+    return iree_make_status(IREE_STATUS_UNIMPLEMENTED,
+                            "(%zu): unimplemented buffer encoding '%s'",
+                            contents_node->start_mark.line, contents_node->tag);
   }
-  iree_hal_buffer_unmap_range(&mapping);
-  return status;
 }
 
 // Writes an element of the given |element_type| with the given integral |value|
@@ -596,60 +590,76 @@ static void iree_trace_replay_generate_fully_specified_pseudorandom_buffer(
 }
 
 // Generates the destination |buffer| using the generator specified by
-// |contents_generator_node|.
+// |generator_node|.
 static iree_status_t iree_trace_replay_generate_hal_buffer(
     iree_trace_replay_t* replay, yaml_document_t* document,
-    yaml_node_t* contents_generator_node, iree_hal_element_type_t element_type,
-    iree_hal_buffer_t* buffer, iree_hal_dim_t* shape,
-    iree_host_size_t shape_size) {
-  if (!contents_generator_node) {
-    return iree_ok_status();
-  } else if (contents_generator_node->type != YAML_SCALAR_NODE) {
+    yaml_node_t* generator_node, iree_hal_element_type_t element_type,
+    const iree_hal_dim_t* shape, iree_host_size_t shape_rank,
+    iree_hal_buffer_mapping_t* mapping) {
+  if (generator_node->type != YAML_SCALAR_NODE) {
     return iree_make_status(
         IREE_STATUS_INVALID_ARGUMENT,
         "(%zu): expected scalar node for buffer contents_generator",
-        contents_generator_node->start_mark.line);
+        generator_node->start_mark.line);
   }
-
-  iree_hal_buffer_mapping_t mapping;
-  IREE_RETURN_IF_ERROR(
-      iree_hal_buffer_map_range(buffer, IREE_HAL_MEMORY_ACCESS_DISCARD_WRITE, 0,
-                                IREE_WHOLE_BUFFER, &mapping));
-  iree_status_t status = iree_ok_status();
-  if (strcmp(contents_generator_node->tag, "!tag:iree:identity_matrix") == 0) {
-    if (shape_size == 2) {
-      iree_hal_dim_t inner_size = shape[shape_size - 1];
-      iree_trace_replay_generate_identity_matrix(element_type, mapping.contents,
-                                                 inner_size);
+  if (strcmp(generator_node->tag, "!tag:iree:identity_matrix") == 0) {
+    if (shape_rank == 2) {
+      iree_hal_dim_t inner_size = shape[shape_rank - 1];
+      iree_trace_replay_generate_identity_matrix(element_type,
+                                                 mapping->contents, inner_size);
     } else {
-      status = iree_make_status(
+      return iree_make_status(
           IREE_STATUS_INVALID_ARGUMENT,
           "the identity_matrix generator is only for 2D shapes (matrices)");
     }
-  } else if (strcmp(contents_generator_node->tag,
+  } else if (strcmp(generator_node->tag,
                     "!tag:iree:fully_specified_pseudorandom") == 0) {
     // To enable pseudorandom tests that are both reproducible and invariant
     // under reordering and filtering testcases, the seed is explicitly
     // passed as argument in the contents_generator tag.
-    iree_string_view_t seed_str = iree_string_view_trim(
-        iree_yaml_node_as_string(contents_generator_node));
-    uint32_t seed;
+    iree_string_view_t seed_str =
+        iree_string_view_trim(iree_yaml_node_as_string(generator_node));
+    uint32_t seed = 0;
     if (iree_string_view_atoi_uint32(seed_str, &seed)) {
       iree_trace_replay_generate_fully_specified_pseudorandom_buffer(
-          element_type, mapping.contents, seed);
+          element_type, mapping->contents, seed);
     } else {
-      status = iree_make_status(IREE_STATUS_INVALID_ARGUMENT,
-                                "could not parse the seed argument ('%s') of "
-                                "the fully_specified_pseudorandom tag",
-                                seed_str.data);
+      return iree_make_status(IREE_STATUS_INVALID_ARGUMENT,
+                              "could not parse the seed argument ('%s') of "
+                              "the fully_specified_pseudorandom tag",
+                              seed_str.data);
     }
   } else {
-    status = iree_make_status(
+    return iree_make_status(
         IREE_STATUS_UNIMPLEMENTED, "(%zu): unimplemented buffer generator '%s'",
-        contents_generator_node->start_mark.line, contents_generator_node->tag);
+        generator_node->start_mark.line, generator_node->tag);
   }
-  iree_hal_buffer_unmap_range(&mapping);
-  return status;
+  return iree_ok_status();
+}
+
+typedef struct iree_trace_replay_generation_params_t {
+  iree_trace_replay_t* replay;
+  yaml_document_t* document;
+  yaml_node_t* contents_node;
+  yaml_node_t* generator_node;
+  iree_hal_element_type_t element_type;
+  const iree_hal_dim_t* shape;
+  iree_host_size_t shape_rank;
+} iree_trace_replay_generation_params_t;
+
+static iree_status_t iree_trace_replay_generate_hal_buffer(
+    iree_hal_buffer_mapping_t* mapping, void* user_data) {
+  iree_trace_replay_generation_params_t* params =
+      (iree_trace_replay_generation_params_t*)user_data;
+  if (params->contents_node) {
+    return iree_trace_replay_parse_hal_buffer(params->replay, params->document,
+                                              params->contents_node,
+                                              params->element_type, mapping);
+  } else {
+    return iree_trace_replay_generate_hal_buffer(
+        params->replay, params->document, params->generator_node,
+        params->element_type, params->shape, params->shape_rank, mapping);
+  }
 }
 
 // Parses a !hal.buffer_view and appends it to |target_list|.
@@ -694,50 +704,37 @@ static iree_status_t iree_trace_replay_parse_hal_buffer_view(
       document, value_node, iree_make_cstring_view("contents"),
       &contents_node));
 
-  yaml_node_t* contents_generator_node = NULL;
+  yaml_node_t* generator_node = NULL;
   IREE_RETURN_IF_ERROR(iree_yaml_mapping_try_find(
       document, value_node, iree_make_cstring_view("contents_generator"),
-      &contents_generator_node));
+      &generator_node));
 
-  if (contents_node && contents_generator_node) {
+  if (contents_node && generator_node) {
     return iree_make_status(
         IREE_STATUS_INVALID_ARGUMENT,
         "(%zu): cannot have both contents and contents_generator",
-        contents_generator_node->start_mark.line);
+        generator_node->start_mark.line);
   }
 
-  iree_device_size_t allocation_size = 0;
-  IREE_RETURN_IF_ERROR(iree_hal_buffer_compute_view_size(
-      shape, shape_rank, element_type, IREE_HAL_ENCODING_TYPE_DENSE_ROW_MAJOR,
-      &allocation_size));
-
-  iree_hal_buffer_t* buffer = NULL;
-  IREE_RETURN_IF_ERROR(iree_hal_allocator_allocate_buffer(
-      iree_hal_device_allocator(replay->device),
-      IREE_HAL_MEMORY_TYPE_DEVICE_LOCAL | IREE_HAL_MEMORY_TYPE_HOST_VISIBLE,
-      IREE_HAL_BUFFER_USAGE_ALL, allocation_size, &buffer));
-  iree_status_t status = iree_trace_replay_generate_hal_buffer(
-      replay, document, contents_generator_node, element_type, buffer, shape,
-      shape_rank);
-  if (!iree_status_is_ok(status)) {
-    iree_hal_buffer_release(buffer);
-    return status;
-  }
-  status = iree_trace_replay_parse_hal_buffer(replay, document, contents_node,
-                                              element_type, buffer);
-  if (!iree_status_is_ok(status)) {
-    iree_hal_buffer_release(buffer);
-    return status;
-  }
-
+  iree_trace_replay_generation_params_t params = {
+      .replay = replay,
+      .document = document,
+      .contents_node = contents_node,
+      .generator_node = generator_node,
+      .element_type = element_type,
+      .shape = shape,
+      .shape_rank = shape_rank,
+  };
   iree_hal_buffer_view_t* buffer_view = NULL;
-  status = iree_hal_buffer_view_create(buffer, shape, shape_rank, element_type,
-                                       encoding_type, &buffer_view);
-  iree_hal_buffer_release(buffer);
-  IREE_RETURN_IF_ERROR(status);
+  IREE_RETURN_IF_ERROR(iree_hal_buffer_view_generate_buffer(
+      iree_hal_device_allocator(replay->device), shape, shape_rank,
+      element_type, encoding_type, IREE_HAL_MEMORY_TYPE_DEVICE_LOCAL,
+      IREE_HAL_BUFFER_USAGE_DISPATCH | IREE_HAL_BUFFER_USAGE_TRANSFER,
+      iree_trace_replay_generate_hal_buffer, &params, &buffer_view));
 
   iree_vm_ref_t buffer_view_ref = iree_hal_buffer_view_move_ref(buffer_view);
-  status = iree_vm_list_push_ref_move(target_list, &buffer_view_ref);
+  iree_status_t status =
+      iree_vm_list_push_ref_move(target_list, &buffer_view_ref);
   iree_vm_ref_release(&buffer_view_ref);
   return status;
 }

GMNGeoffrey/iree-clean

iree/tools/utils/trace_replay.c
@@ -468,7 +468,7 @@ static iree_status_t iree_trace_replay_parse_hal_encoding_type(
 static iree_status_t iree_trace_replay_parse_hal_buffer(
     iree_trace_replay_t* replay, yaml_document_t* document,
     yaml_node_t* contents_node, iree_hal_element_type_t element_type,
-    iree_hal_buffer_t* buffer) {
+    iree_hal_buffer_mapping_t* mapping) {
   if (!contents_node) {
     // Empty contents = zero fill.
     return iree_ok_status();
@@ -480,23 +480,17 @@ static iree_status_t iree_trace_replay_parse_hal_buffer(
   iree_string_view_t value =
       iree_string_view_trim(iree_yaml_node_as_string(contents_node));
 
-  iree_hal_buffer_mapping_t mapping;
-  IREE_RETURN_IF_ERROR(
-      iree_hal_buffer_map_range(buffer, IREE_HAL_MEMORY_ACCESS_DISCARD_WRITE, 0,
-                                IREE_WHOLE_BUFFER, &mapping));
   iree_status_t status = iree_ok_status();
   if (strcmp(contents_node->tag, "tag:yaml.org,2002:binary") == 0) {
-    status = iree_yaml_base64_decode(value, mapping.contents);
+    return iree_yaml_base64_decode(value, mapping->contents);
   } else if (strcmp(contents_node->tag, "tag:yaml.org,2002:str") == 0) {
-    status =
-        iree_hal_parse_buffer_elements(value, element_type, mapping.contents);
+    return iree_hal_parse_buffer_elements(value, element_type,
+                                          mapping->contents);
   } else {
-    status = iree_make_status(
-        IREE_STATUS_UNIMPLEMENTED, "(%zu): unimplemented buffer encoding '%s'",
-        contents_node->start_mark.line, contents_node->tag);
+    return iree_make_status(IREE_STATUS_UNIMPLEMENTED,
+                            "(%zu): unimplemented buffer encoding '%s'",
+                            contents_node->start_mark.line, contents_node->tag);
   }
-  iree_hal_buffer_unmap_range(&mapping);
-  return status;
 }
 
 // Writes an element of the given |element_type| with the given integral |value|
@@ -596,60 +590,76 @@ static void iree_trace_replay_generate_fully_specified_pseudorandom_buffer(
 }
 
 // Generates the destination |buffer| using the generator specified by
-// |contents_generator_node|.
+// |generator_node|.
 static iree_status_t iree_trace_replay_generate_hal_buffer(
     iree_trace_replay_t* replay, yaml_document_t* document,
-    yaml_node_t* contents_generator_node, iree_hal_element_type_t element_type,
-    iree_hal_buffer_t* buffer, iree_hal_dim_t* shape,
-    iree_host_size_t shape_size) {
-  if (!contents_generator_node) {
-    return iree_ok_status();
-  } else if (contents_generator_node->type != YAML_SCALAR_NODE) {
+    yaml_node_t* generator_node, iree_hal_element_type_t element_type,
+    const iree_hal_dim_t* shape, iree_host_size_t shape_rank,
+    iree_hal_buffer_mapping_t* mapping) {
+  if (generator_node->type != YAML_SCALAR_NODE) {
     return iree_make_status(
         IREE_STATUS_INVALID_ARGUMENT,
         "(%zu): expected scalar node for buffer contents_generator",
-        contents_generator_node->start_mark.line);
+        generator_node->start_mark.line);
   }
-
-  iree_hal_buffer_mapping_t mapping;
-  IREE_RETURN_IF_ERROR(
-      iree_hal_buffer_map_range(buffer, IREE_HAL_MEMORY_ACCESS_DISCARD_WRITE, 0,
-                                IREE_WHOLE_BUFFER, &mapping));
-  iree_status_t status = iree_ok_status();
-  if (strcmp(contents_generator_node->tag, "!tag:iree:identity_matrix") == 0) {
-    if (shape_size == 2) {
-      iree_hal_dim_t inner_size = shape[shape_size - 1];
-      iree_trace_replay_generate_identity_matrix(element_type, mapping.contents,
-                                                 inner_size);
+  if (strcmp(generator_node->tag, "!tag:iree:identity_matrix") == 0) {
+    if (shape_rank == 2) {
+      iree_hal_dim_t inner_size = shape[shape_rank - 1];
+      iree_trace_replay_generate_identity_matrix(element_type,
+                                                 mapping->contents, inner_size);
     } else {
-      status = iree_make_status(
+      return iree_make_status(
           IREE_STATUS_INVALID_ARGUMENT,
           "the identity_matrix generator is only for 2D shapes (matrices)");
     }
-  } else if (strcmp(contents_generator_node->tag,
+  } else if (strcmp(generator_node->tag,
                     "!tag:iree:fully_specified_pseudorandom") == 0) {
     // To enable pseudorandom tests that are both reproducible and invariant
     // under reordering and filtering testcases, the seed is explicitly
     // passed as argument in the contents_generator tag.
-    iree_string_view_t seed_str = iree_string_view_trim(
-        iree_yaml_node_as_string(contents_generator_node));
-    uint32_t seed;
+    iree_string_view_t seed_str =
+        iree_string_view_trim(iree_yaml_node_as_string(generator_node));
+    uint32_t seed = 0;
     if (iree_string_view_atoi_uint32(seed_str, &seed)) {
       iree_trace_replay_generate_fully_specified_pseudorandom_buffer(
-          element_type, mapping.contents, seed);
+          element_type, mapping->contents, seed);
     } else {
-      status = iree_make_status(IREE_STATUS_INVALID_ARGUMENT,
-                                "could not parse the seed argument ('%s') of "
-                                "the fully_specified_pseudorandom tag",
-                                seed_str.data);
+      return iree_make_status(IREE_STATUS_INVALID_ARGUMENT,
+                              "could not parse the seed argument ('%s') of "
+                              "the fully_specified_pseudorandom tag",
+                              seed_str.data);
     }
   } else {
-    status = iree_make_status(
+    return iree_make_status(
         IREE_STATUS_UNIMPLEMENTED, "(%zu): unimplemented buffer generator '%s'",
-        contents_generator_node->start_mark.line, contents_generator_node->tag);
+        generator_node->start_mark.line, generator_node->tag);
   }
-  iree_hal_buffer_unmap_range(&mapping);
-  return status;
+  return iree_ok_status();
+}
+
+typedef struct iree_trace_replay_generation_params_t {
+  iree_trace_replay_t* replay;
+  yaml_document_t* document;
+  yaml_node_t* contents_node;
+  yaml_node_t* generator_node;
+  iree_hal_element_type_t element_type;
+  const iree_hal_dim_t* shape;
+  iree_host_size_t shape_rank;
+} iree_trace_replay_generation_params_t;
+
+static iree_status_t iree_trace_replay_generate_hal_buffer(
+    iree_hal_buffer_mapping_t* mapping, void* user_data) {
+  iree_trace_replay_generation_params_t* params =
+      (iree_trace_replay_generation_params_t*)user_data;
+  if (params->contents_node) {
+    return iree_trace_replay_parse_hal_buffer(params->replay, params->document,
+                                              params->contents_node,
+                                              params->element_type, mapping);
+  } else {
+    return iree_trace_replay_generate_hal_buffer(
+        params->replay, params->document, params->generator_node,
+        params->element_type, params->shape, params->shape_rank, mapping);
+  }
 }
 
 // Parses a !hal.buffer_view and appends it to |target_list|.
@@ -694,51 +704,46 @@ static iree_status_t iree_trace_replay_parse_hal_buffer_view(
       document, value_node, iree_make_cstring_view("contents"),
       &contents_node));
 
-  yaml_node_t* contents_generator_node = NULL;
+  yaml_node_t* generator_node = NULL;
   IREE_RETURN_IF_ERROR(iree_yaml_mapping_try_find(
       document, value_node, iree_make_cstring_view("contents_generator"),
-      &contents_generator_node));
+      &generator_node));
 
-  if (contents_node && contents_generator_node) {
+  if (contents_node && generator_node) {
     return iree_make_status(
         IREE_STATUS_INVALID_ARGUMENT,
         "(%zu): cannot have both contents and contents_generator",
-        contents_generator_node->start_mark.line);
+        generator_node->start_mark.line);
+  } else if (contents_node || generator_node) {
+    iree_trace_replay_generation_params_t params = {
+        .replay = replay,
+        .document = document,
+        .contents_node = contents_node,
+        .generator_node = generator_node,
+        .element_type = element_type,
+        .shape = shape,
+        .shape_rank = shape_rank,
+    };
+    IREE_RETURN_IF_ERROR(iree_hal_buffer_view_generate_buffer(
+        iree_hal_device_allocator(replay->device), shape, shape_rank,
+        element_type, encoding_type,
+        IREE_HAL_MEMORY_TYPE_DEVICE_LOCAL | IREE_HAL_MEMORY_TYPE_HOST_VISIBLE,
+        IREE_HAL_BUFFER_USAGE_DISPATCH | IREE_HAL_BUFFER_USAGE_TRANSFER |
+            IREE_HAL_BUFFER_USAGE_MAPPING,
+        iree_trace_replay_generate_hal_buffer_callback, &params, &buffer_view));
+  } else {
+    IREE_RETURN_IF_ERROR(iree_hal_buffer_view_allocate_buffer(
+        iree_hal_device_allocator(replay->device), shape, shape_rank,
+        element_type, encoding_type,
+        IREE_HAL_MEMORY_TYPE_DEVICE_LOCAL | IREE_HAL_MEMORY_TYPE_HOST_VISIBLE,
+        IREE_HAL_BUFFER_USAGE_DISPATCH | IREE_HAL_BUFFER_USAGE_TRANSFER |
+            IREE_HAL_BUFFER_USAGE_MAPPING,
+        iree_const_byte_span_empty(), &buffer_view));
   }
-
-  iree_device_size_t allocation_size = 0;
-  IREE_RETURN_IF_ERROR(iree_hal_buffer_compute_view_size(
-      shape, shape_rank, element_type, IREE_HAL_ENCODING_TYPE_DENSE_ROW_MAJOR,
-      &allocation_size));
-
-  iree_hal_buffer_t* buffer = NULL;
-  IREE_RETURN_IF_ERROR(iree_hal_allocator_allocate_buffer(
-      iree_hal_device_allocator(replay->device),
-      IREE_HAL_MEMORY_TYPE_DEVICE_LOCAL | IREE_HAL_MEMORY_TYPE_HOST_VISIBLE,
-      IREE_HAL_BUFFER_USAGE_ALL, allocation_size, &buffer));
-  iree_status_t status = iree_trace_replay_generate_hal_buffer(
-      replay, document, contents_generator_node, element_type, buffer, shape,
-      shape_rank);
-  if (!iree_status_is_ok(status)) {
-    iree_hal_buffer_release(buffer);
-    return status;
-  }
-  status = iree_trace_replay_parse_hal_buffer(replay, document, contents_node,
-                                              element_type, buffer);
-  if (!iree_status_is_ok(status)) {
-    iree_hal_buffer_release(buffer);
-    return status;
-  }
-
-  iree_hal_buffer_view_t* buffer_view = NULL;
-  status = iree_hal_buffer_view_create(buffer, shape, shape_rank, element_type,
-                                       encoding_type, replay->host_allocator,
-                                       &buffer_view);
-  iree_hal_buffer_release(buffer);
-  IREE_RETURN_IF_ERROR(status);
 
   iree_vm_ref_t buffer_view_ref = iree_hal_buffer_view_move_ref(buffer_view);
-  status = iree_vm_list_push_ref_move(target_list, &buffer_view_ref);
+  iree_status_t status =
+      iree_vm_list_push_ref_move(target_list, &buffer_view_ref);
   iree_vm_ref_release(&buffer_view_ref);
   return status;
 }